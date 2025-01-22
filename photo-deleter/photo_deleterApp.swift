49459cfae176508758ab130755348d04559f4365
Tom Planche <Tomplanche@icloud.com>
2025-01-22 19:48:02 +0100

    Initial Commit

---
+++ photo-deleter/photo_deleterApp.swift
@@ -0,0 +1,32 @@
+//
+//  photo_deleterApp.swift
+//  photo-deleter
+//
+//  Created by Tom Planche on 22/01/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct photo_deleterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ photo-deleterTests/photo_deleterTests.swift
@@ -0,0 +1,17 @@
+//
+//  photo_deleterTests.swift
+//  photo-deleterTests
+//
+//  Created by Tom Planche on 22/01/2025.
+//
+
+import Testing
+@testable import photo_deleter
+
+struct photo_deleterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
